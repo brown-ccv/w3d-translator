180ab58f9d4eaa18c163ee4c1b4dcbe426ea10d4
RobertGemmaJr <rgemma.jr@gmail.com>
2022-04-01 17:49:26 -0400

    feat: validate input and output files

---
+++ w3d_translator/validate.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import shutil
+from tkinter import N
+
+# Ensure input folder is a directory
+# TODO: More dir validation?
+def validate_input(dir):
+    if not os.path.isdir(dir):
+        sys.exit(f"Error: INPUT is not a directory: {dir}")
+    
+    return
+
+# Creates output directory, if valid
+# TODO: More dir validation?
+def validate_output(dir, force):
+    try: os.mkdir(dir)
+    except FileExistsError:
+        if force:
+            # Remove and make directory
+            shutil.rmtree(dir)
+            os.mkdir(dir)
+        else: 
+            sys.exit(
+                f"Error: OUTPUT directory already exists: {dir}.\n" + 
+                "Use --force to overwrite"
+            )
--- w3d_translator/w3d_translator.py
+++ w3d_translator/w3d_translator.py
@@ -1,12 +1,35 @@
 import typer
 from unity import UNITY_PATH, UNITY_VERSION
+from validate import validate_input, validate_output
 
-
-# Opening Message
+# Opening message
 def greeting():
     print("W3D TRANSLATOR")
     print(f"Unity Version:\t {UNITY_VERSION}")
-    print(f'Please ensure Unity is installed at {UNITY_PATH}\n')  
+    print(f'Please ensure Unity is installed at {UNITY_PATH}\n')
+
+def main(
+    input: str,             # Folder containing the xml project
+    output: str,            # Output folder for the translated project
+    multiple: bool = False,  # Translate multiple projects?
+    force: bool = False,     # Overwrite contents out output?
+):
+    """
+    Translate W3D projects from INPUT to Unity projects in OUTPUT
+    
+    If --multiple: individual projects are subfolders of INPUT and OUTPUT
+    """
+
+    # Print greeting and validate parameters
+    greeting()
+    validate_input(input)
+    validate_output(output, force)
+
+    # if multiple:
+    #     # TODO Validate input for all subfolders
+    #     print(multiple)
+    # else:
+    #     print(multiple)
 
 if __name__ == '__main__':
-    greeting()
+    typer.run(main)
