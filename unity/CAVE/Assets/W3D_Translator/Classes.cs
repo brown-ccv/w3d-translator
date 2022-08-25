66368bb6a535a769a72d8dbdbcabf3bf886527f7
RobertGemmaJr <rgemma.jr@gmail.com>
2022-08-25 17:43:49 -0400

    style: spaceing

--- unity/CAVE/Assets/W3D_Translator/Classes.cs
+++ unity/CAVE/Assets/W3D_Translator/Classes.cs
@@ -246,7 +246,7 @@ namespace XML
             tmpText.horizontalAlignment = (HorizontalAlignmentOptions)HorizontalAlignment;
             tmpText.verticalAlignment = (VerticalAlignmentOptions)VerticalAlignment;
             tmpText.color = color; // Vertex Color
-            
+
             // Load font material
             // TODO (72): More robust path checking
             TMP_FontAsset tmpFont = Resources.Load<TMP_FontAsset>(
