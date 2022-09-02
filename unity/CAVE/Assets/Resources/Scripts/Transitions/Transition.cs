32d463c5ad9f6514eb77f849d297b2865a4aff12
RobertGemmaJr <rgemma.jr@gmail.com>
2022-09-02 15:51:39 -0400

    chore: commented out code

--- unity/CAVE/Assets/Resources/Scripts/Transitions/Transition.cs
+++ unity/CAVE/Assets/Resources/Scripts/Transitions/Transition.cs
@@ -19,22 +19,6 @@ namespace Writing3D
         {
             [SerializeField] public float Duration;
 
-            // public UnityAction<Actions.Object> GetUnityAction(GameObject reference)
-            // {
-            //     ObjectManager script = reference.GetComponent<ObjectManager>();
-            //     return this switch
-            //     {
-            //         Visible => new UnityAction<Actions.Object>(script.VisibleTransition),
-            //         Move => new UnityAction<Actions.Object>(script.MoveTransition),
-            //         RelativeMove => new UnityAction<Actions.Object>(script.RelativeMoveTransition),
-            //         Color => new UnityAction<Actions.Object>(script.ColorTransition),
-            //         Scale => new UnityAction<Actions.Object>(script.ScaleTransition),
-            //         Sound => new UnityAction<Actions.Object>(script.SoundTransition),
-            //         Link => new UnityAction<Actions.Object>(script.LinkTransition),
-            //         _ => null, // force error
-            //     };
-            // }
-
             public UnityAction<Transition> GetUnityAction(GameObject reference)
             {
                 ObjectManager script = reference.GetComponent<ObjectManager>();
