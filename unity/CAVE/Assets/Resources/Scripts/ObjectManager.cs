670d7a16d93a13c061c95775ff5f7527a8cd67b1
RobertGemmaJr <rgemma.jr@gmail.com>
2022-09-14 15:11:57 -0400

    feat: Add transition to change color

--- unity/CAVE/Assets/Resources/Scripts/ObjectManager.cs
+++ unity/CAVE/Assets/Resources/Scripts/ObjectManager.cs
@@ -1,6 +1,10 @@
-using UnityEngine;
+using System;
+using System.Collections;
 
-using Writing3D.Transitions;
+using UnityEngine;
+using TMPro;
+
+using Writing3D;
 
 namespace Writing3D
 {
@@ -8,14 +12,24 @@ namespace Writing3D
     {
         public ContentTypes Type;
 
-        // Update the GameObject's color
-        public void SetColor(UnityEngine.Color color)
+        public enum ContentTypes
         {
-            Debug.Log($"{name} {Type}");
+            Object, // None
+            Text,
+            Image,
+            StereoImage,
+            Model,
+            Light,
+            ParticleSystem,
+        }
+
+        // Update the GameObject's color
+        public void SetColor(Color color)
+        {
             switch (Type)
             {
                 case ContentTypes.Text:
-                    GetComponent<TMPro.TextMeshPro>().color = color;
+                    GetComponent<TextMeshPro>().color = color;
                     break;
                 case ContentTypes.Image: // TODO 65
                 case ContentTypes.StereoImage: // TODO 66
@@ -29,6 +43,22 @@ namespace Writing3D
             }
         }
 
+        // Find the correct color component to change
+        public dynamic GetColorComponent()
+        {
+            return Type switch
+            {
+                ContentTypes.Text => GetComponent<TextMeshPro>(),
+                ContentTypes.Image => GetComponent<Renderer>(), // TODO 65
+                ContentTypes.StereoImage => GetComponent<Renderer>(), // TODO 66
+                ContentTypes.Model => GetComponent<Renderer>(), // TODO 67
+                ContentTypes.Light => GetComponent<Renderer>(), // TODO 68
+                ContentTypes.ParticleSystem => GetComponent<Renderer>(), // TODO 69
+                ContentTypes.Object => GetComponent<Renderer>(), // TODO
+                _ => null,
+            };
+        }
+
         /* Transitions */
 
         // TODO 121: ObjectManager functions
@@ -36,63 +66,64 @@ namespace Writing3D
         // e.g. ColorTransition should set text color
 
         // TODO 126: Pass derived class directly? Would have to change GetUnityAction
-        public void VisibleTransition(Transition transition)
+        public void VisibleTransition(Transitions.Transition transition)
         {
-            var visible = transition as Visible;
-            Debug.Log($"Visible {gameObject.name} {visible.Enabled}");
+            var visible = transition as Transitions.Visible;
+            // Debug.Log($"Visible {gameObject.name} {visible.Enabled}");
 
             // Fade In/Out and enable/disable the GameObject
             // https://owlcation.com/stem/How-to-fade-out-a-GameObject-in-Unity
             GetComponent<Renderer>().enabled = GetComponent<Collider>().enabled = visible.Enabled;
         }
 
-        public void MoveTransition(Transition transition)
+        public void MoveTransition(Transitions.Transition transition)
         {
             // Update parent
             // MoveTowards && RotateTowards in local space (parent space?)
-            var move = transition as Move;
-            Debug.Log($"RelativeMove {gameObject.name} {move.Parent.name} {move.Position}");
+            var move = transition as Transitions.Move;
+            // Debug.Log($"RelativeMove {gameObject.name} {move.Parent.name} {move.Position}");
         }
 
-        public void RelativeMoveTransition(Transition transition)
+        public void RelativeMoveTransition(Transitions.Transition transition)
         {
-            RelativeMove move = transition as RelativeMove;
-            Debug.Log($"RelativeMove {gameObject.name} {move.Parent.name} {move.Position}");
+            var move = transition as Transitions.RelativeMove;
+            // Debug.Log($"RelativeMove {gameObject.name} {move.Parent.name} {move.Position}");
         }
 
-        public void ColorTransition(Transition transition)
+        public void ColorTransition(Transitions.Transition transition)
         {
-            var color = transition as Transitions.Color;
-            Debug.Log($"Color {gameObject.name} {color.NewColor}");
+            StartCoroutine(ChangeColor(transition as Transitions.Color));
+
+            IEnumerator ChangeColor(Transitions.Color transition)
+            {
+                var component = GetColorComponent();
+                var startColor = component.color;
+                float t = 0;
+                while (t < 1)
+                {
+                    t += Time.deltaTime / transition.Duration;
+                    component.color = Color.Lerp(startColor, transition.NewColor, t);
+                    yield return null;
+                }
+            }
         }
 
-        public void ScaleTransition(Transition transition)
+        public void ScaleTransition(Transitions.Transition transition)
         {
-            var scale = transition as Scale;
-            Debug.Log($"Scale {gameObject.name} {scale.NewScale}");
+            var scale = transition as Transitions.Scale;
+            // Debug.Log($"Scale {gameObject.name} {scale.NewScale}");
         }
 
-        public void SoundTransition(Transition transition)
+        public void SoundTransition(Transitions.Transition transition)
         {
-            var sound = transition as Sound;
-            Debug.Log($"Sound {gameObject.name} {sound.Operation}");
+            var sound = transition as Transitions.Sound;
+            // Debug.Log($"Sound {gameObject.name} {sound.Operation}");
         }
 
-        public void LinkTransition(Transition transition)
+        public void LinkTransition(Transitions.Transition transition)
         {
-            var link = transition as Link;
-            Debug.Log($"Link {gameObject.name} {link.Operation}");
-        }
-
-        public enum ContentTypes
-        {
-            Object, // None
-            Text,
-            Image,
-            StereoImage,
-            Model,
-            Light,
-            ParticleSystem,
+            var link = transition as Transitions.Link;
+            // Debug.Log($"Link {gameObject.name} {link.Operation}");
         }
     }
 }
