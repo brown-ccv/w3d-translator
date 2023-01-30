e345d7fd6ac54940339bd971057066798c6c0d19
RobertGemmaJr <rgemma.jr@gmail.com>
2023-01-30 16:52:53 -0500

    feat: Fix compilator option logic, disable XR and MVR in CAVE scene

--- unity/CAVE/Assets/Resources/Scripts/SceneManager.cs
+++ unity/CAVE/Assets/Resources/Scripts/SceneManager.cs
@@ -7,7 +7,6 @@ namespace Writing3D
         public GameObject MVR;   // MVRManager root object
         public GameObject XR;    // XR Origin root object
 
-        // Called when GameObject is initialized
         protected void Awake()
         {
             // TODO: Document that XR and MVR must be disabled in the editor
@@ -15,29 +14,22 @@ namespace Writing3D
             // In editor - activate XR
             XR.SetActive(true);
             MVR.SetActive(false);
-#endif
-
-#if UNITY_STANDALONE
+#elif UNITY_STANDALONE
             // In Windows executable - activate MVR
             XR.SetActive(false);
             MVR.SetActive(true);
-#endif
-
-#if UNITY_ANDROID
+#elif UNITY_ANDROID
             // In Android executable - activate XR
             XR.SetActive(true);
             MVR.SetActive(false);
 #endif
         }
 
-        // Called on the frame a script it enabled (after Awake)
         protected void Start()
         {
-#if UNITY_STANDALONE
             // If MVR is disabled we must activate XR
             // MVR will disable itself if called without a config file (MVRManagerScript.Awake())
             XR.SetActive(!MVR.activeInHierarchy);
-#endif
         }
 
         protected void Update()
